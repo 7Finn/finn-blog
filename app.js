567db4c9cf62cb9b0530f2f0eab6e1dcf59108eb
57Finn <wu7finn@gmail.com>
2017-03-14 18:58:52 +0800

    修复了生产环境中的Bug

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var webpack = require('webpack');
 var favicon = require('serve-favicon');
 var history = require('connect-history-api-fallback');
 var logger = require('morgan');
@@ -28,22 +27,21 @@ module.exports = function(db) {
   }));
 
 
-  // 使用 webpack-dev-middleware 中间件
   if (app.get('env') == 'development') {
     var webpack = require('webpack');
-    var webpackConfig = require('./build/webpack.dev.config');
+    var webpackConfig = require('./build/webpack.dev.conf');
        // 调用webpack并把配置传递过去
     var compiler = webpack(webpackConfig);
       // 使用 webpack-dev-middleware 中间件
     var devMiddleware = require('webpack-dev-middleware')(compiler, {
        publicPath: '/',
-       stats: {
-         colors: true,
-         chunks: false
-       }
+       stats: {colors: true, chunks: false    }
     });
     app.use(devMiddleware);
     app.use(require("webpack-hot-middleware")(compiler));
+
+  } else{
+    app.use(express.static('output'));
   }
 
 
